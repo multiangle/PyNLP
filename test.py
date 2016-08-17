a382a7c981d95b174e9e7e114e6b9b33f96447a6
multiangle <461445957@qq.com>
2016-08-18 06:51:10 +0800

    csdn asyncio测试

--- test.py
+++ test.py
@@ -1,17 +1,59 @@
-import jieba
-import re
+import urllib.request as request
+from bs4 import BeautifulSoup as bs
+import asyncio
+import aiohttp
+
+@asyncio.coroutine
+async def getPage(url,res_list):
+    print(url)
+    headers = {'User-Agent':'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'}
+    # conn = aiohttp.ProxyConnector(proxy="http://127.0.0.1:8087")
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url,headers=headers) as resp:
+            assert resp.status==200
+            res_list.append(await resp.text())
+
+def parse_list_page(page_str):
+    page = bs(page_str,'lxml')
+    # 获取文章链接
+    articles = page.find_all('div',attrs={'class':'article_title'})
+    art_urls = []
+    for a in articles:
+        x = a.find('a')['href']
+        art_urls.append('http://blog.csdn.net'+x)
+    return art_urls
+
+class parseListPage():
+    def __init__(self,page_str):
+        self.page_str = page_str
+    def __enter__(self):
+        page_str = self.page_str
+        page = bs(page_str,'lxml')
+        # 获取文章链接
+        articles = page.find_all('div',attrs={'class':'article_title'})
+        art_urls = []
+        for a in articles:
+            x = a.find('a')['href']
+            art_urls.append('http://blog.csdn.net'+x)
+        return art_urls
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
 
 
-def etl(s): #remove 标点和特殊字符
-    s = re.sub(r'\u3000','',s)
-    return s
-a = """北京体育局辟谣没收到张怡宁退役申请 太不符逻辑
-　　本报讯(记者 崔方舟)刚刚在新闻发布会上说暂不退役的张怡宁，近日又“被爆出”已经递交退役申请的消息。昨日，北京市体育局竞技处负责人孙国华出面否定了这一说法，“我们从来就没有收到过张怡宁的退役申请。”
-　　对于前天马来西亚中文媒体表示张怡宁已经递交退役申请的报道，孙国华直言“不可能”。对于这样的消息孙国华也表示非常无奈，“前几天张怡宁才开了新闻发布会说不退役，她不可能刚说完不退役就递交申请吧，想想也知道不符合逻辑。”据悉，按照正常程序，由于张怡宁的档案关系仍挂在先农坛体校，如果她要退役，首先要向体校递交退役申请。先农坛体校校长孟强华也表示，自全运会结束后张怡宁从未向队中递交退役申请，而北京队教练张雷也称没有听说张怡宁向队中申请退役。
-"""
-a_split = a.split('\n')
-a_res = map(etl,a_split)
-x = filter(lambda x:x.__len__()>0, a_res)
-for i in x:
-    print(i)
-# b = jieba.cut(a,cut_all=False)
+page_num = 5
+page_url_base = 'http://blog.csdn.net/u014595019/article/list/'
+page_urls = [page_url_base + str(i+1) for i in range(page_num)]
+loop = asyncio.get_event_loop()
+ret_list = []
+tasks = [getPage(host,ret_list) for host in page_urls]
+loop.run_until_complete(asyncio.wait(tasks))
+
+articles_url = []
+for ret in ret_list:
+    with parseListPage(ret) as tmp:
+        articles_url += tmp
+ret_list = []
+
+tasks = [getPage(url, ret_list) for url in articles_url]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
