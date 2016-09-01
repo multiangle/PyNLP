d126f5cf4089a13af0395d9219827dca35410dec
multiangle <461445957@qq.com>
2016-09-01 22:41:59 +0800

    先完成LSI模型

--- test/gensim_test/THU_LSI.py
+++ test/gensim_test/THU_LSI.py
@@ -81,88 +81,108 @@ def generate_dict_subprocess(id,
                 print(file[:min(file.__len__(),5)])
     dict_queue.put(dictionary)
 
+def genDict(path_parent, path_dict_folder):
+    # 第一次遍历，成立词典，获取词频，文频等信息
+    p_pool = []
+    dict_queue = Queue()
+    p_num = 3
+    for i in range(p_num):
+        p = Process(target=generate_dict_subprocess,
+                    args=(i,
+                          p_num,
+                          dict_queue,
+                          cut_all,
+                          # '/mnt/D/multiangle/DataSet/THUCNews'
+                          path_parent
+                          ))
+        p_pool.append(p)
+
+    for p in p_pool: # 启动进程
+        # p = Process(p)
+        p.start()
+
+    while True: # 检测是否全部完成
+        if dict_queue.qsize() >= p_num:
+            break
+        time.sleep(1)
+
+    dictionary = corpora.Dictionary()
+    for i in range(p_num):
+        q_dict = dict_queue.get()
+        dictionary.merge_with(q_dict)
+
+    for p in p_pool:
+        p.terminate()
+
+    dictionary.save(os.path.join(path_dict_folder,'THUNews.dict'))
+    dictionary.save_as_text(os.path.join(path_dict_folder,'THUNews.txt'))
+
+def convDoc2Vector(path_doc_parent,path_dict_folder,path_root):
+    dictionary = corpora.Dictionary.load(os.path.join(path_dict_folder,'THUNews_picked.dict'))
+    print(os.path.join(path_dict_folder,'THUNews_picked.dict'))
+    for folder in loadFolders(path_doc_parent):
+        folder_name = folder.split('/')[-1]
+        print(folder_name)
+        files = os.listdir(folder)
+        cate_bow = []
+        count = 0
+        for file in files:
+            count += 1
+            if count%100 == 0 :
+                print('{c} at {t}'.format(c=count, t=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime())))
+            if count%10 > 0: # 抽样 n抽1
+                continue
+            file_path = os.path.join(folder,file)
+            file = open(file_path,'rb')
+            doc = file.read().decode('utf8')
+            word_list = convert_doc_to_wordlist(doc, cut_all)
+            word_bow = dictionary.doc2bow(word_list)
+            cate_bow.append(word_bow)
+            file.close()
+
+        tmp_path = os.path.join(path_root,'bow_sampling')
+        if not os.path.exists(tmp_path):
+            os.mkdir(tmp_path)
+        corpora.MmCorpus.serialize(tmp_path+'/{x}.mm'.format(x=folder_name),
+                                   cate_bow,
+                                   id2word=dictionary,
+                                   # labels=folder_name
+                                   )
+
 if __name__=='__main__':
 
     cut_all = True # 是否要把所有可能的单词都列出来？ true 表示是 , false 表示否
-    par_path = '/media/multiangle/OS/THUCNews'
+    path_root   = '/media/multiangle/F/DataSet/THUCNews'
+    path_doc_parent = os.path.join(path_root,'THUCNewsTotal')
+    os.chdir(path_doc_parent)
+    path_dict_folder = os.path.join(path_root, 'THUNewsDict') # 存放词典的地方
+    if not os.path.exists(path_dict_folder):
+        os.mkdir(path_dict_folder)
 
-    # # # ===================================================================
+
+    # # ===================================================================
     # # 第一次遍历，成立词典，获取词频，文频等信息
-    # p_pool = []
-    # dict_queue = Queue()
-    # p_num = 3
-    # for i in range(p_num):
-    #     p = Process(target=generate_dict_subprocess,
-    #                 args=(i,
-    #                       p_num,
-    #                       dict_queue,
-    #                       cut_all,
-    #                       # '/mnt/D/multiangle/DataSet/THUCNews'
-    #                       par_path
-    #                       ))
-    #     p_pool.append(p)
-    #
-    # for p in p_pool: # 启动进程
-    #     # p = Process(p)
-    #     p.start()
-    #
-    # while True: # 检测是否全部完成
-    #     if dict_queue.qsize() >= p_num:
-    #         break
-    #     time.sleep(1)
-    #
-    # dictionary = corpora.Dictionary()
-    # for i in range(p_num):
-    #     q_dict = dict_queue.get()
-    #     dictionary.merge_with(q_dict)
-    #
-    # for p in p_pool:
-    #     p.terminate()
-    #
-    # dictionary.save('./THUNews_multiprocess.dict')
-    # dictionary.save_as_text('./THUNews_dict_multiprocess.txt')
+    # genDict(path_doc_parent, path_dict_folder)
 
-    # # # ===================================================================
+    # # ===================================================================
     # # 去掉词典中出现次数过少的
-    # dictionary = corpora.Dictionary.load('./THUNews_multiprocess.dict')
+    # dictionary = corpora.Dictionary.load(os.path.join(path_dict_folder, 'THUNews.dict'))
     # small_freq_ids = [tokenid for tokenid, docfreq in dictionary.dfs.items() if docfreq < 5 ]
     # dictionary.filter_tokens(small_freq_ids)
     # dictionary.compactify()
-    # dictionary.save('./THUNews_picked.dict')
+    # dictionary.save(os.path.join(path_dict_folder, 'THUNews_picked.dict'))
 
-    # # # ===================================================================
+    # # ===================================================================
     # # 第二次遍历，开始将文档转化成id稀疏表示
-    # dictionary = corpora.Dictionary.load('./THUNews_picked.dict')
-    # for folder in loadFolders('/media/multiangle/OS/THUCNews'):
-    #     folder_name = folder.split('/')[-1]
-    #     files = os.listdir(folder)
-    #     cate_bow = []
-    #     count = 0
-    #     for file in files:
-    #         count += 1
-    #         if count%100 == 0 :
-    #             print('{c} at {t}'.format(c=count, t=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime())))
-    #         if count%10 > 0: # 抽样 n抽1
-    #             continue
-    #         file_path = os.path.join(folder,file)
-    #         file = open(file_path,'rb')
-    #         doc = file.read().decode('utf8')
-    #         word_list = convert_doc_to_wordlist(doc, cut_all)
-    #         word_bow = dictionary.doc2bow(word_list)
-    #         cate_bow.append(word_bow)
-    #         file.close()
-    #
-    #     corpora.MmCorpus.serialize('./bow_sampling/{x}.mm'.format(x=folder_name),
-    #                                cate_bow,
-    #                                id2word=dictionary,
-    #                                labels=folder_name,
-    #                                )
+    # convDoc2Vector(path_doc_parent, path_dict_folder, path_root)
 
     # # # ===================================================================
     # # 第三次遍历，开始将文档转化成tf idf 表示
-    # dictionary = corpora.Dictionary.load('./THUNews_picked.dict')
-    # bow_path = './bow_sampling'
-    # tfidf_path = './tfidf_sampling'
+    # dictionary = corpora.Dictionary.load(os.path.join(path_dict_folder,'THUNews_picked.dict'))
+    # bow_path = path_root + '/bow_sampling'
+    # tfidf_path = path_root + '/tfidf_sampling'
+    # if not os.path.exists(tfidf_path):
+    #     os.mkdir(tfidf_path)
     # files = os.listdir(bow_path)
     # cate_set = set([x.split('.')[0] for x in files])
     # for cat in cate_set:
@@ -175,13 +195,15 @@ if __name__=='__main__':
     #                                corpus_tfidf,
     #                                id2word=dictionary
     #                                )
-    #     # print('{f}/{c}.mm'.format(f=tfidf_path,c=cat))
+    #     print('{f}/{c}.mm'.format(f=tfidf_path,c=cat))
 
-    # # ===================================================================
-    # 第四次遍历，计算lsi
-    dictionary = corpora.Dictionary.load('./THUNews_picked.dict')
-    tfidf_path = './tfidf_sampling'
-    lsi_path = './lsi_sampling'
+    # # # ===================================================================
+    # # 第四次遍历，计算lsi
+    dictionary  = corpora.Dictionary.load(os.path.join(path_dict_folder,'THUNews_picked.dict'))
+    tfidf_path  = path_root + '/tfidf_sampling'
+    lsi_path    = path_root + '/lsi_sampling'
+    if not os.path.exists(lsi_path):
+        os.mkdir(lsi_path)
     files = os.listdir(tfidf_path)
     cate_list = list(set([x.split('.')[0] for x in files]))
     doc_num_list = []
@@ -196,7 +218,7 @@ if __name__=='__main__':
             tfidf_corpus_total += [x for x in corpus]
         print('category {c} loaded,len {l} at {t}'
               .format(c=cat,l=corpus.num_docs,t=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime())))
-    lsi_model = models.LsiModel(corpus = tfidf_corpus_total, dictionary = dictionary, num_topics=10)
+    lsi_model = models.LsiModel(corpus = tfidf_corpus_total, id2word = dictionary, num_topics=10)
     print('lsi model is generated at {t}'.format(t=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime())))
     del tfidf_corpus_total  # 总共的tfidf corpus已经用完，释放变量空间
     for cat in cate_list:
