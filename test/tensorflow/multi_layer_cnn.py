fa61158c9102238acf07c2ad2f52fbf28369380e
multiangle <461445957@qq.com>
2016-09-20 14:46:43 +0800

    tensorflow 试用

---
+++ test/tensorflow/multi_layer_cnn.py
@@ -0,0 +1,123 @@
+# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""A very simple MNIST classifier.
+See extensive documentation at
+http://tensorflow.org/tutorials/mnist/beginners/index.md
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# Import data
+from tensorflow.examples.tutorials.mnist import input_data
+
+import tensorflow as tf
+
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+flags.DEFINE_string('data_dir', '/tmp/data/', 'Directory for storing data')
+
+print(FLAGS.data_dir)
+mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
+print(mnist)
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                          strides=[1, 2, 2, 1], padding='SAME')
+
+sess = tf.InteractiveSession()
+
+x = tf.placeholder(tf.float32, [None, 784])
+W_conv1 = weight_variable([5, 5, 1, 32])  # 卷积是在每个5*5的patch中算出32个特征，分别是patch大小，输入通道数目，输出通道数目
+b_conv1 = bias_variable([32])
+x_image = tf.reshape(x, [-1,28,28,1]) #为了用这一层，我们把x变成一个 4d 向量，第 2、3 维对应图片的宽高，最后一维代表颜色通道
+h_conv1 = tf.nn.elu(conv2d(x_image, W_conv1) + b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
+
+W_conv2 = weight_variable([5, 5, 32, 64])
+b_conv2 = bias_variable([64])
+
+h_conv2 = tf.nn.elu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+
+W_fc1 = weight_variable([7 * 7 * 64, 1024])
+b_fc1 = bias_variable([1024])
+
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_fc1 = tf.nn.elu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+W_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
+
+y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+y_ = tf.placeholder(tf.float32, [None, 10])
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+sess.run(tf.initialize_all_variables())
+for i in range(20000):
+    batch = mnist.train.next_batch(50)
+    if i%100 == 0:
+        print(batch[0].__len__())
+        train_accuracy = accuracy.eval(feed_dict={
+            x:batch[0], y_: batch[1], keep_prob: 1.0})
+        print("step %d, training accuracy %g"%(i, train_accuracy))
+    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+print("test accuracy %g"%accuracy.eval(feed_dict={
+    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+
+# # Create the model
+#
+# x = tf.placeholder(tf.float32, [None, 784])
+# W = tf.Variable(tf.zeros([784, 10]))
+# b = tf.Variable(tf.zeros([10]))
+# y = tf.nn.softmax(tf.matmul(x, W) + b)
+#
+#
+# # Define loss and optimizer
+# y_ = tf.placeholder(tf.float32, [None, 10])
+# cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+# train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+#
+# # Train
+# sess.run(tf.initialize_all_variables())
+#
+# for i in range(1000):
+#     batch_xs, batch_ys = mnist.train.next_batch(100)
+#     # print(batch_ys)
+#     train_step.run({x: batch_xs, y_: batch_ys})
+#
+# # Test trained model
+# correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+# accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+# print(accuracy.eval({x: mnist.test.images, y_: mnist.test.labels}))
---
+++ test/tensorflow/simple_classify_demo.py
@@ -0,0 +1,88 @@
+import numpy as np
+import random
+import math
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+def gen_sample():
+    data = []
+    radius = [0,50]
+    for i in range(1000):  # 生成10k个点
+        catg = random.randint(0,1)  # 决定分类
+        r = random.random()*10
+        arg = random.random()*360
+        len = r + radius[catg]
+        x_c = math.cos(math.radians(arg))*len
+        y_c = math.sin(math.radians(arg))*len
+        x = random.random()*30 + x_c
+        y = random.random()*30 + y_c
+        data.append([x,y,catg])
+    return data
+
+def plot_dots(data):
+    data_asclass = [[] for i in range(2)]
+    for d in data:
+        data_asclass[int(d[2])].append((d[0],d[1]))
+    colors = ['r.','b.','r.','b.']
+    for i,d in enumerate(data_asclass):
+        # print(d)
+        nd = np.array(d)
+        plt.plot(nd[:,0],nd[:,1],colors[i])
+    plt.draw()
+
+if __name__=='__main__':
+    data = gen_sample()
+    data = np.array(data)
+    input_dim = 2
+    output_dim = 2
+    hidden_size = 100
+
+    x = tf.placeholder(tf.float32,[None, input_dim])
+    y = tf.placeholder(tf.float32,[None, output_dim])
+    W_h = tf.Variable(tf.truncated_normal([input_dim,hidden_size],stddev=0.1))
+    b_h = tf.Variable(tf.constant(0.1, shape=[hidden_size]))
+    W_y = tf.Variable(tf.truncated_normal([hidden_size,output_dim],stddev=0.1))
+    b_y = tf.Variable(tf.constant(0.1, shape=[output_dim]))
+
+
+    h = tf.nn.sigmoid((tf.matmul(x,W_h)+b_h))  # mul 是点乘， matmul才是矩阵乘法
+    a = tf.nn.softmax(tf.matmul(h,W_y)+b_y)
+
+    loss = tf.reduce_mean(tf.square(a-y))
+    optimizer = tf.train.GradientDescentOptimizer(0.5)
+    train = optimizer.minimize(loss)
+
+    init = tf.initialize_all_variables()
+    sess = tf.Session()
+    sess.run(init)
+
+    tags = data[:,2]
+    output = np.zeros([data.__len__(),2])
+    for i,tag in enumerate(tags):
+        output[i,tag] = 1
+    train_data = data[0:800,0:2]
+    train_out = output[0:800,:]
+    test_data = data[800:1000,0:2]
+    test_out = output[800:1000,:]
+
+    correct_prediction = tf.equal(tf.argmax(a,1),tf.argmax(y,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+
+    for i in range(1000):
+        batch = 10
+        start = random.randint(0,train_data.__len__()-batch-1)
+        input = train_data[start:start+batch,:]
+        output = train_out[start:start+batch,:]
+        sess.run(train, feed_dict={x:input, y:output})
+        if i%100==0:
+            accuracy.eval(feed_dict={x:train_data,y:train_out})
+
+
+
+
+    #
+    #
+    #
+    # plot_dots(data)
+    # plt.show()
